ef978344902025ef1767a18d964447278bd7f470
Artem <cherniavsky.frontend@gmail.com>
2019-03-05 02:40:00 +0300

    change basedir for server

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -3,7 +3,7 @@ const browserSync = require('browser-sync').create();
 module.exports = () => {
     browserSync.init({
         server: {
-            baseDir: './dev'
+            baseDir: 'dev'
         }
     });
 };
