f37eb26d90f69b7ae82600167346f5b5115fa981
Artem <cherniavsky.frontend@gmail.com>
2019-03-05 03:32:37 +0300

    fix server

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 const gulp = require('gulp');
 const server = require('./gulp/tasks/server');
-const html = require('./gulp/tasks/html');4
-
-console.log(html)
+const html = require('./gulp/tasks/html');
 
 gulp.task('default', gulp.series(html, server));
